d1f57dfe6fe1acaf114baee22610cd51f51ad47c
tungxuan <tungxuan@tungxuans-Mac-mini.local>
2024-04-02 23:48:35 +0700

    feat: escrow token

--- programs/escrow/src/lib.rs
+++ programs/escrow/src/lib.rs
@@ -1,15 +1,32 @@
 use anchor_lang::prelude::*;
+mod contexts;
+use contexts::*;
+mod states;
 
 declare_id!("8gc7QHvsreuPTUrYpJ7dJxr6c8UB8kapeLW9Uoot8G1g");
 
 #[program]
-pub mod escrow {
+pub mod anchor_escrow {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        Ok(())
+    pub fn initialize(
+        ctx: Context<Initialize>,
+        seed: u64,
+        initializer_amount: u64,
+        taker_amount: u64,
+    ) -> Result<()> {
+        ctx.accounts
+            .initialize_escrow(seed, &ctx.bumps, initializer_amount, taker_amount)?;
+        ctx.accounts.deposit(initializer_amount)
+        ctx.accounts.
+    }
+
+    pub fn cancel(ctx: Context<Cancel>) -> Result<()> {
+        ctx.accounts.refund_and_close_vault()
+    }
+
+    pub fn exchange(ctx: Context<Exchange>) -> Result<()> {
+        ctx.accounts.deposit()?;
+        ctx.accounts.withdraw_and_close_vault()
     }
 }
-
-#[derive(Accounts)]
-pub struct Initialize {}
