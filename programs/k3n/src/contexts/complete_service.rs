0e068c5e1cf7e4c34c321ec660141cf20819d812
tungxuan <tungxuan@tungxuans-Mac-mini.local>
2024-04-05 13:57:28 +0700

    gen idl

--- programs/k3n/src/contexts/complete_service.rs
+++ programs/k3n/src/contexts/complete_service.rs
@@ -5,12 +5,12 @@ use anchor_lang::solana_program::system_instruction;
 
 #[derive(Accounts)]
 pub struct CompleteService<'info> {
-    #[account(mut, has_one = hirer)]
+    #[account(mut, constraint = service.hirer.key() == hirer.key(), has_one = hirer)]
     pub service: Account<'info, Service>,
     /// CHECK: This is safe because we're explicitly checking the hirer's pubkey in the function
     #[account(signer)]
     pub hirer: AccountInfo<'info>,
-    /// The recipient of the SOL transfer.
+    /// CHECK: The recipient of the SOL transfer.
     #[account(mut)]
     pub kol: AccountInfo<'info>,
     pub system_program: Program<'info, System>,
@@ -18,7 +18,6 @@ pub struct CompleteService<'info> {
 
 impl<'info> CompleteService<'info> {
     pub fn internal_complete_service(&mut self) -> Result<()> {
-        // Verify that the signer is the hirer
         require!(
             self.hirer.key() == self.service.hirer.key(),
             K3NError::Permission
@@ -27,20 +26,26 @@ impl<'info> CompleteService<'info> {
             self.kol.key() == self.service.kol.key(),
             K3NError::CheckKolAddressFail
         );
-        // Perform the SOL transfer from hirer to kol
+
         let transfer_ix = system_instruction::transfer(
             &self.hirer.key(),
             &self.kol.key(),
             self.service.service_fee,
         );
-        invoke(
+
+        match invoke(
             &transfer_ix,
             &[self.hirer.to_account_info(), self.kol.to_account_info()],
-        )?;
-
-        // Update the `is_completed` field
-        self.service.is_completed = true;
-
-        Ok(())
+        ) {
+            Ok(_) => {
+                self.service.is_completed = true;
+                Ok(())
+            }
+            Err(e) => {
+                // Handle or log the error more specifically here
+                msg!("Transfer failed with error: {:?}", e);
+                Err(e.into())
+            }
+        }
     }
 }
